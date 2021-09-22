1358f5222f458392e0a373b4053e78b6289e98ba
Piotr <piotro@arg.pl>
2021-09-22 11:03:58 +0200

    initial commit

---
+++ src/test/java/org/piotr/businessmanagementsystem/BusinessManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package org.piotr.businessmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusinessManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
